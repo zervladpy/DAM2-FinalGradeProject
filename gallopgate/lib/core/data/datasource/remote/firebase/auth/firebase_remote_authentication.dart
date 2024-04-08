8284e9dc9a4aef281764ddb4e3ee3e28bd001f42
Vladislav Zernov <zernovvladislav7@gmail.com>
2024-04-08 13:09:35 +0200

    -r changed throws on authentication to follow `AuthException` rules

--- gallopgate/lib/core/data/datasource/remote/firebase/auth/firebase_remote_authentication.dart
+++ gallopgate/lib/core/data/datasource/remote/firebase/auth/firebase_remote_authentication.dart
@@ -1,12 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:gallopgate/core/domain/datasource/auth/i_auth_remote_datasource.dart';
 import 'package:gallopgate/core/domain/models/model_user.dart';
+import 'package:gallopgate/utils/error/auth/auth_exeption.dart';
 
 class FirebaseRemoteAuthentication extends IAuthRemoteDatasource {
 
   FirebaseRemoteAuthentication(this.firebaseAuth);
 
-  final firebase_auth.FirebaseAuth firebaseAuth;
+  final firebase_auth.FirebaseAuth firebaseAuth; 
 
   User? _user;
 
@@ -16,8 +17,18 @@ class FirebaseRemoteAuthentication extends IAuthRemoteDatasource {
 
   @override
   Future<User?> loginWithEmailAndPassword(String email, String password) async {
-    var credentaials = await firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
-    return fromFirebaseUser(credentaials.user);
+    try {
+      var credentaials = await firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+      return fromFirebaseUser(credentaials.user);
+    } on firebase_auth.FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case 'invalid-email' : throw AuthLoginException.invalidEmail();
+        case 'wrong-password' : throw AuthLoginException.invalidPassword();
+        case 'user-disabled' : throw AuthLoginException.userDisabled();
+        case 'user-not-found' : throw AuthLoginException.userNotFound();
+        default : throw AuthLoginException.unkonwn();
+      }
+    }
   }
 
   @override
@@ -28,8 +39,18 @@ class FirebaseRemoteAuthentication extends IAuthRemoteDatasource {
 
   @override
   Future<User?> signupWithEmailAndPassword(String email, String password) async {
-    var credentaials = await firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
-    return fromFirebaseUser(credentaials.user);
+    try {
+      var credentaials = await firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
+      return fromFirebaseUser(credentaials.user);
+    } on firebase_auth.FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case 'invalid-email' : throw AuthSignupException.invalidEmail();
+        case 'email-already-in-use' : throw AuthSignupException.emailInUse();
+        case 'operation-not-allowed' : throw AuthSignupException.operationNotAllowed();
+        case 'weak-password' : throw AuthSignupException.weekPassword();
+        default : throw AuthLoginException.unkonwn();
+      }
+    }
   }
 
   User? fromFirebaseUser(firebase_auth.User? user) {
