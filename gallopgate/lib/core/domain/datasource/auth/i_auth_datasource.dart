9d68aa029af0e70c6c8e93bcf37a3dcf1f7aa201
Vladislav Zernov <zernovvladislav7@gmail.com>
2024-04-13 20:38:43 +0200

    -a auth `datasource` and `repository`

--- gallopgate/lib/core/domain/datasource/auth/i_auth_datasource.dart
+++ gallopgate/lib/core/domain/datasource/auth/i_auth_datasource.dart
@@ -2,7 +2,6 @@ import 'package:gallopgate/core/domain/models/model_user.dart';
 
 /// Contract for AuthDatasource
 abstract class IAuthDatasource {
-
   IAuthDatasource();
 
   /// Current instance of [User]
@@ -12,7 +11,7 @@ abstract class IAuthDatasource {
   /// - [email] - String
   /// - [password] - String
   ///
-  /// Returns: 
+  /// Returns:
   /// - [User] if SUCCESS
   /// - null if FAILURE
   Future<User?> loginWithEmailAndPassword(String email, String password);
@@ -21,12 +20,14 @@ abstract class IAuthDatasource {
   /// - [email] - String
   /// - [password] - String
   ///
-  /// Returns: 
+  /// Returns:
   /// - [User] if SUCCESS
   /// - null if FAILURE
   Future<User?> signupWithEmailAndPassword(String email, String password);
-  
+
   /// Logs [User] out
   Future<void> logout();
 
+  /// Gets a authentication status [Stream]
+  Stream<bool> get userAuthenticationStream;
 }
--- gallopgate/lib/core/domain/repository/auth/i_auth_repository.dart
+++ gallopgate/lib/core/domain/repository/auth/i_auth_repository.dart
@@ -2,16 +2,13 @@ import 'package:gallopgate/core/domain/models/model_user.dart';
 
 /// Contract for in app authorization
 abstract class IAuthRepository {
+  User? get currentUser;
 
-  User? user;
-
-  User? get currentUser => user;
-
-  bool get isAuthenticated => user != null;
+  bool get isAuthenticated;
 
   Future<User?> loginWithEmailAndPassword(String email, String password);
 
   Future<User?> signupWithEmailAndPassword(String email, String password);
-  
+
   Future<void> logout();
 }
