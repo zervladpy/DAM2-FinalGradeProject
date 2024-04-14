821ea63d7036e2bf548940048c1e17214ddf0a4e
Vladislav Zernov <zernovvladislav7@gmail.com>
2024-04-14 13:27:41 +0200

    -f removed unused import

--- gallopgate/lib/features/auth/login/domain/usecase/usecase_login.dart
+++ gallopgate/lib/features/auth/login/domain/usecase/usecase_login.dart
@@ -1,4 +1,3 @@
-import 'package:gallopgate/core/data/repository/auth/firebase_repository_authentication.dart';
 import 'package:gallopgate/core/domain/datasource/auth/i_auth_remote_datasource.dart';
 import 'package:gallopgate/core/domain/models/model_user.dart';
 import 'package:gallopgate/utils/error/auth/auth_exeption.dart';
@@ -7,7 +6,6 @@ import 'package:gallopgate/utils/usecase/usecase.dart';
 
 /// Usecase for login
 class LoginUsecase extends UseCase<User, LoginUsecaseParams> {
-
   const LoginUsecase(this.repository);
 
   final IAuthRemoteDatasource repository;
@@ -15,21 +13,20 @@ class LoginUsecase extends UseCase<User, LoginUsecaseParams> {
   @override
   Future<Response<User>> call(LoginUsecaseParams params) async {
     try {
-      var user = await repository.loginWithEmailAndPassword(params.email, params.password);
+      var user = await repository.loginWithEmailAndPassword(
+          params.email, params.password);
       return Response.success(user!);
     } on AuthLoginException catch (e) {
       return Response.failure(e.error);
     }
   }
-
 }
 
 /// Parameters for LoginUsecase
-/// 
+///
 /// - [email] - String
 /// - [password] - String
 class LoginUsecaseParams extends UsecaseParams {
-
   const LoginUsecaseParams(this.email, this.password);
 
   final String email, password;
